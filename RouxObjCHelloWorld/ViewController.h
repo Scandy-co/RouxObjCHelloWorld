6fac791b0ec4c3e04f0d4a4744dec500e91c1ce1
April Polubiec <aprilpolubiec@gmail.com>
2020-07-22 15:50:07 -0500

    add UI elements

--- RouxObjCHelloWorld/ViewController.h
+++ RouxObjCHelloWorld/ViewController.h
@@ -11,12 +11,24 @@
 
 @interface ViewController : GLKViewController
 
-@property (weak, nonatomic) IBOutlet UIButton *connectToMirrorDeviceButton;
-@property (weak, nonatomic) IBOutlet UITextField *IPAddressInput;
-@property (weak, nonatomic) IBOutlet UILabel *IPAddressLabel;
-@property (weak, nonatomic) IBOutlet UILabel *connectedNetworkLabel;
+//Scanning Device UI
+@property (weak, nonatomic) IBOutlet UIView *scanningDeviceView;
 @property (weak, nonatomic) IBOutlet UIButton *changeHostButton;
-@property (weak, nonatomic) IBOutlet UIButton *restartScannerButton;
+@property (weak, nonatomic) IBOutlet UIButton *startPreviewButton;
+@property (weak, nonatomic) IBOutlet UIButton *saveMeshButton;
+@property (weak, nonatomic) IBOutlet UITextField *IPAddressInput;
+@property (weak, nonatomic) IBOutlet UILabel *IPAddressInputLabel;
+@property (weak, nonatomic) IBOutlet UIButton *connectToMirrorDeviceButton;
+
+//Mirror Device UI
+@property (weak, nonatomic) IBOutlet UIView *mirrorDeviceView;
+@property (weak, nonatomic) IBOutlet UIButton *startScanButton;
+@property (weak, nonatomic) IBOutlet UIButton *stopScanButton;
+@property (weak, nonatomic) IBOutlet UILabel *scanSizeLabel;
+@property (weak, nonatomic) IBOutlet UISlider *scanSizeSlider;
+@property (weak, nonatomic) IBOutlet UISwitch *v2ModeSwitch;
+@property (weak, nonatomic) IBOutlet UILabel *v2ModeLabel;
+
 
 @end
 
